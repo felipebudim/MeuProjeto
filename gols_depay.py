25252c13895e2d2311b816903ce2c57ae4947481
Felipe Budim <febudims@hotmail.com>
2024-09-20 14:50:52 -0300

    alterou titulo risos

--- gols_depay.py
+++ gols_depay.py
@@ -1,6 +1,7 @@
+#Gols do depay
 print("vamos ver quantos gols o depay têm?")
-Gols = input("informe quantos gols o depay têm ")
-print(f"o depay tem {Gols} gols pelo corinthians, com certeza está mandando muito bem")
+Gols = input("informe quantos gols o depay têm, por favor ")
+print(f"o depay tem {Gols} gols pelo sport clube corinthians, com certeza está mandando muito bem, não acham")
 
 
 
